5ac39d4d3dcc03fd7c9a1899f1912357520e0bd1
Artem  F <artemfm@gmail.com>
2021-02-03 20:54:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing (line){
 function currentLine(line){
   var n = `The line is currently:`
   var str = []
-  if (line.lenght === 0) return "The line is currently empty."
+  if (line.length === 0) return "The line is currently empty."
   else{
     for (let i = 0; i < line.length; i++)
     { str[i] = `${[i+1]}. ${line[i]}`;}
