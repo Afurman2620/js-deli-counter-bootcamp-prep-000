0c10ce4c13f3b82d6cf9f7635a977bdc0db13cf3
Artem  F <artemfm@gmail.com>
2021-02-03 20:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function currentLine(line){
     { deliLine[i] = `${[i+1]}. ${line[i]}`;}
     
   }
-  return `The line is currently: ${deliLine}`
+  return `The line is currently: ${deliLine .join}`
 }
