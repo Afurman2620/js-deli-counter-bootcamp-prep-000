82c95625abc8a6d540389fc61a5d50f6175253a2
Artem  F <artemfm@gmail.com>
2021-02-03 20:20:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function currentLine(line){
   var deliLine = []
   if (line.lenght === 0) return "The line is currently empty."
   else{
-    for (let )
+    for (let i = 0; i < line.length; i++){
+        newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;}
   }
 }
